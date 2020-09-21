ccdeab0d758ce996403705a9443b747b2b71b269
Alexander <malveiralexander@outlook.com>
2020-09-21 12:18:03 -0300

    Primer commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+import Server from './classes/server';
+import router from './routes/router';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const server = new Server();
+
+
+// bodyParse
+
+server.app.use(bodyParser.urlencoded({extended: true}));
+server.app.use(bodyParser.json());
+
+// Cors
+server.app.use(cors({ origin: true, credentials: true }))
+
+server.app.use('/', router);
+
+server.start(()=> {
+    console.log(`Corriendo en el puerto ${server.port}`)
+});
